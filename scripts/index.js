0dcdf1c4bb09de1b9f8b1d7a85fc8639e8f6ba47
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2024-02-24 23:22:34 -0600

    Creates initial content

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+/**
+ * Namespace for index.html
+ */
+registerNamespace("Pages.Index", function (ns)
+{
+
+});
+
+/**
+ * Code to be called when the window first loads
+ */
+window.onload = () =>
+{
+	Common.loadTheme();
+	Common.setUpAccessibility();
+	Common.Components.registerShortcuts({
+		//"ALT+H": {
+		//	action: () => { document.getElementById("homeLink").click(); },
+		//	description: "Return to the home page"
+		//},
+		"ALT+S": {
+			action: () => { document.getElementById("shortcutsButton").click(); },
+			description: "Show shortcut keys"
+		},
+	});
+};
