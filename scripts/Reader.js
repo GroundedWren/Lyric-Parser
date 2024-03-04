06fccbd15242147311cdf995461e8393da4587d1
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2024-03-03 18:56:52 -0600

    Creates the reader page control

--- scripts/Reader.js
+++ scripts/Reader.js
@@ -1,6 +1,37 @@
 registerNamespace("LyricParser.Pages.Reader", function (ns)
 {
+	ns.changeDiscography = () =>
+	{
+		const selection = document.getElementById("selDiscography").value;
+		if (selection === "UPLOAD")
+		{
+			document.getElementById("discographyUpload").classList.remove("hidden");
+			return;
+		}
+		else
+		{
+			document.getElementById("discographyUpload").classList.add("hidden");
+		}
+		Common.FileLib.loadJSONFileFromDirectory(selection).then(discObj =>
+		{
+			if (!discObj) { return; }
+			ns.discographyUploaded(discObj);
+		});
+	};
 
+	ns.onFileUpload = (changeEv) =>
+	{
+		Common.FileLib.parseJSONFile(
+			changeEv.target.files[0],
+			ns.discographyUploaded
+		);
+	};
+
+	ns.discographyUploaded = (discObj) =>
+	{
+		LyricParser.Pages.Reader.mainPageCtrl.enableTabs();
+		LyricParser.Pages.Reader.mainPageCtrl.setActiveTab("mainPageCtrl_tab_Collections");
+	};
 });
 
 window.onload = () =>
@@ -13,4 +44,19 @@ window.onload = () =>
 			description: "Show shortcut keys"
 		},
 	});
+
+	LyricParser.Pages.Reader.mainPageCtrl = new Common.Controls.PageControl.PageControl(
+		document.getElementById("mainPageCtrl"),
+		document.getElementById("mainPageCtrl_ts"),
+		document.getElementById("mainPageCtrl_pgc"),
+		{
+			"mainPageCtrl_tab_Collections": document.getElementById("mainPageCtrl_page_Collections"),
+			"mainPageCtrl_tab_Tracks": document.getElementById("mainPageCtrl_page_Tracks"),
+			"mainPageCtrl_tab_Search": document.getElementById("mainPageCtrl_page_Search"),
+		},
+		"Select a discography to begin",
+		"Reader",
+		document.getElementById("mainPageCtrl_gutter")
+	);
+	LyricParser.Pages.Reader.mainPageCtrl.disableTabs();
 };
