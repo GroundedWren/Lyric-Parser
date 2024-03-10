403de5f75c445f879d8bacdc0b5a214b366bd3e6
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2024-03-10 16:43:10 -0500

    Slightly corrects n' pre-stemming

--- scripts/LyricParser.js
+++ scripts/LyricParser.js
@@ -2,15 +2,22 @@ registerNamespace("LyricParser", function (ns)
 {
 	ns.preStemWord = (word) =>
 	{
-		return word.toLowerCase().replace(
+		let preStemmedWord = word.toLowerCase().replace(
 			/\u003f|!|\u002e|,|\u0022|\u0028|\u0029|'s/g,
 			""
 		).replace(
 			/\u2019|\u2018/g,
 			"'"
-		).replace(
-			/n'/g,
-			"ng"
 		);
+
+		if (preStemmedWord.endsWith("n'"))
+		{
+			preStemmedWord = preStemmedWord.replace(
+				/n'/g,
+				"ng"
+			);
+		}
+
+		return preStemmedWord
 	};
 });
